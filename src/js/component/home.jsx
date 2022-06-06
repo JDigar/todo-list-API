423300b741c5abab3728814aae07ca335d67564b
JDigar <jdigar96@gmail.com>
2022-06-06 08:21:29 +0000

    no funciona nada

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,6 +3,28 @@ import React, { useState } from "react";
 import TaskList from "./taskList.jsx";
 import Tarea from "./tarea.jsx";
 
+// fetch("https://assets.breatheco.de/apis/fake/todos/user/jonathandiaz", {
+// 	method: "GET",
+// 	body: JSON.stringify(todos),
+// 	headers: {
+// 		"Content-Type": "application/json",
+// 	},
+// })
+// 	.then((resp) => {
+// 		console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
+// 		console.log(resp.status); // el código de estado = 200 o código = 400 etc.
+// 		console.log(resp.text()); // Intentará devolver el resultado exacto como cadena (string)
+// 		return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+// 	})
+// 	.then((data) => {
+// 		//Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+// 		console.log(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
+// 	})
+// 	.catch((error) => {
+// 		//manejo de errores
+// 		console.log(error);
+// 	});
+
 //create your first component
 const Home = () => {
 	const [listaTareas, setListaTareas] = useState([]);
